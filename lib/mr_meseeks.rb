1459a7efc78ccf20504f5c97abd52241a0db1272
kenneth peter jones <kenjones620@yahoo.co.uk>
2017-11-17 22:57:58 +0000

    extract calendar logic out into a wrapper

--- lib/mr_meseeks.rb
+++ lib/mr_meseeks.rb
@@ -1,21 +1,16 @@
 require 'gmail'
 require 'date'
-require 'google/apis/calendar_v3'
-require 'googleauth'
-require 'googleauth/stores/file_token_store'
 require 'json'
 require 'fileutils'
 require 'pry'
 require_relative 'config.rb'
 require_relative 'event_data.rb'
+require_relative 'calendar_service.rb'
 require_relative 'patches/object.rb'
 
 class MrMeseeks
 	def initialize
-		# Initialize the API
-		@calendar = Google::Apis::CalendarV3::CalendarService.new
-		@calendar.client_options.application_name = Config::APPLICATION_NAME
-		@calendar.authorization = authorize_calendar
+		@calendar = CalendarService.new.calendar
 	end
 
 	def scan_emails
@@ -36,31 +31,6 @@ class MrMeseeks
     date = Date.parse(array[2].split[0...4].join(' '))
     make_event(date, band, mail)
 	end
-
-	def authorize_calendar
-		oob_uri = 'urn:ietf:wg:oauth:2.0:oob'
-		scope = Google::Apis::CalendarV3::AUTH_CALENDAR
-		
-		FileUtils.mkdir_p(File.dirname(Config::CREDENTIALS_PATH))
-
-	  client_id = Google::Auth::ClientId.from_file(Config::CLIENT_SECRETS_PATH)
-	  token_store = Google::Auth::Stores::FileTokenStore.new(file: Config::CREDENTIALS_PATH)
-	  authorizer = Google::Auth::UserAuthorizer.new(
-	    client_id, scope, token_store)
-	  user_id = 'default'
-	  credentials = authorizer.get_credentials(user_id)
-	  if credentials.nil?
-	    url = authorizer.get_authorization_url(
-	      base_url: oob_uri)
-	    puts "Open the following URL in the browser and enter the " +
-	         "resulting code after authorization"
-	    puts url
-	    code = gets
-	    credentials = authorizer.get_and_store_credentials_from_code(
-	      user_id: user_id, code: code, base_url: oob_uri)
-	  end
-  	credentials
-	end 
 
 	def event_exists?(data, band)
 		exists = false
