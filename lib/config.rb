976e9fda626a16c4bcb4f09a1c8bcddea857cb67
kenneth peter jones <kenjones620@yahoo.co.uk>
2017-11-19 23:32:30 +0000

    switch to env vars

--- lib/config.rb
+++ lib/config.rb
@@ -2,8 +2,8 @@ require 'yaml'
 
 module Config
   data = YAML.load_file('./config/secrets.yml')
-  UNAME = data['uname']
-  PWORD = data['pword']
+  UNAME = ENV['GMAIL_UNAME']
+  PWORD = ENV['GMAIL_PWORD']
   APPLICATION_NAME = 'NellyMail'
   CLIENT_SECRETS_PATH = data['client_secrets_path']
   CREDENTIALS_PATH = data['credentials_path']
